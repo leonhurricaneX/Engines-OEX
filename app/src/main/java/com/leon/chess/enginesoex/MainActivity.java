d32bdc8e1dd2a89be6886f5019a315e95d68b2f8
leonhurricaneX <gamertatta@gmail.com>
2022-06-20 20:17:33 +0530

    initial commit

---
+++ app/src/main/java/com/leon/chess/enginesoex/MainActivity.java
@@ -0,0 +1,19 @@
+package com.leon.chess.enginesoex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+    }
+}
+
+
+
---
+++ app/src/main/java/com/leon/chess/enginesoex/StockfishEngineProvider.java
@@ -0,0 +1,8 @@
+package com.leon.chess.enginesoex;
+
+
+
+import com.kalab.chess.enginesupport.ChessEngineProvider;
+
+public class StockfishEngineProvider extends ChessEngineProvider {
+}
